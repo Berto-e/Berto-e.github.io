efaee4cf623a6f7cc0ef859a167f561059924c33
Alberto Álvarez <alber.alvagon@alum.uca.es>
2024-02-02 13:46:38 +0100

    fix animation

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.getElementById("spellbook").addEventListener("click", function () {
   let panelSkills = document.getElementById("skillPanel-2");
-  panelSkills.style.height =
-    panelSkills.style.height == 0 || panelSkills.style.height == "0px"
-      ? "200px"
-      : "0px";
+  panelSkills.style.display =
+    panelSkills.style.display == "none" 
+      ? "block"
+      : "none";
 });
 
