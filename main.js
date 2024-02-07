59e503f06a83873db6bd09efd6b13001d80ee27a
bert0h <albertoalvarezinfo@gmail.com>
2024-02-07 10:40:59 +0100

    fix skill-set animation

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 document.getElementById("spellbook").addEventListener("click", function () {
   let panelSkills = document.getElementById("skillPanel-2");
-  
-    if(panelSkills.style.display == "none"){
-      panelSkills.style.display = "block";
-      panelSkills.style.height = "200px";
-    }
-    else{
-      panelSkills.style.display = "none";
-      setTimeout(() => {
-        panelSkills.style.height = "0px";
-      }, 150);
-    }
-});
 
+  if (panelSkills.style.display == "none") {
+    panelSkills.style.display = "block";
+    panelSkills.style.height = "200px";
+  } else {
+    panelSkills.style.height = "0px";
+
+    setTimeout(() => {
+      panelSkills.style.display = "none";
+    }, 250);
+  }
+});
